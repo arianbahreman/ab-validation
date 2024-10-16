eae50326b551ed14a901d578245807f6031fab34
Arian Bahreman <arian.bahreman@gmail.com>
2024-10-16 19:54:23 +0330

    fix: added type for value

--- src/validators/email.ts
+++ src/validators/email.ts
@@ -1,6 +1,6 @@
 import createValidator from "../create-validator";
 
-const Email = createValidator("email", (value) => {
+const Email = createValidator("email", (value: string) => {
   return /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim.test(
     String(value)
   );
