4c006ec9ee4204a406585ded644eed137cc4ef6e
Arian Bahreman <arian.bahreman@gmail.com>
2024-10-16 16:02:18 +0330

    feat: better email validation

--- src/validators/email.ts
+++ src/validators/email.ts
@@ -1,7 +1,9 @@
-import createValidator from "../create-validator"
+import createValidator from "../create-validator";
 
 const Email = createValidator("email", (value) => {
-  return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,20}$/g.test(String(value))
-})
+  return /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim.test(
+    String(value)
+  );
+});
 
-export default Email
+export default Email;
