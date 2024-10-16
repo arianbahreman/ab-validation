a82bc1f986c9860334e5b577aa2feb49077bb930
Arian Bahreman <arian.bahreman@gmail.com>
2024-10-16 16:11:07 +0330

    fix: correct min and max length options behavior

--- src/validators/text.ts
+++ src/validators/text.ts
@@ -1,16 +1,16 @@
-import createValidator from "../create-validator"
+import createValidator from "../create-validator";
 
 interface TextOptions {
-  minLength?: number
-  maxLength?: number
+  minLength?: number;
+  maxLength?: number;
 }
 
 const Text = createValidator<TextOptions>("text", (value, options) => {
-  const text = String(value)
+  const text = String(value);
   return (
-    text.length > (options?.minLength ?? 0) &&
-    text.length < (options?.maxLength ?? Number.MAX_SAFE_INTEGER)
-  )
-})
+    text.length >= (options?.minLength ?? 0) &&
+    text.length <= (options?.maxLength ?? Number.MAX_SAFE_INTEGER)
+  );
+});
 
-export default Text
+export default Text;
