7e10b7aa380cf60d8f857fead34a1058bb485143
Arian Bahreman <arian.bahreman@gmail.com>
2024-10-17 12:50:50 +0330

    feat: validators exported

---
+++ src/validators/validators.ts
@@ -0,0 +1,7 @@
+import Required from "./required";
+import Email from "./email";
+import List from "./list";
+import Pattern from "./pattern";
+import Text from "./text";
+
+export { Required, Email, List, Pattern, Text };
