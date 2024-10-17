4c3adb555cc4dd020eba298f03bc97dec38a20c9
Arian Bahreman <arian.bahreman@gmail.com>
2024-10-17 11:37:00 +0330

    fix: value type fixed

--- src/validators/email.ts
+++ src/validators/email.ts
@@ -1,6 +1,6 @@
 import createValidator from "../create-validator";
 
-const Email = createValidator("email", (value: string) => {
+const Email = createValidator("email", (value: unknown) => {
   return /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim.test(
     String(value)
   );
