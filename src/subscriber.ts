535c5eac152ced62071566185c8f397339f69a13
Arian Bahreman <arian.bahreman@gmail.com>
2024-10-17 11:37:28 +0330

    feat: init subscriber

---
+++ src/subscriber.ts
@@ -0,0 +1,21 @@
+export default function subscriber() {
+  const subscribers: CallableFunction[] = [];
+  /**
+   * Subscribe
+   */
+  const subscribe = (callback: (result: unknown) => void) => {
+    subscribers.push(callback);
+    return function unsubscribe() {
+      const index = subscribers.indexOf(callback);
+      if (index !== -1) {
+        subscribers.splice(index, 1);
+      }
+    };
+  };
+
+  const dispatch = (result: unknown) => {
+    subscribers.forEach((callback) => callback(result));
+  };
+
+  return { subscribe, dispatch };
+}
